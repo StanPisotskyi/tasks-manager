9f5ae356941ca2356f8d2ac186db7eb54153bc1d
Stanislav Pisotskyi <spisotskyi@gmail.com>
2024-02-16 09:23:02 +0200

    init

---
+++ src/main/java/tasks/manager/api/controllers/PingController.java
@@ -0,0 +1,14 @@
+package tasks.manager.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import tasks.manager.api.dto.Ping;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public Ping ping() {
+        return new Ping(true);
+    }
+}
---
+++ src/main/java/tasks/manager/api/dto/Ping.java
@@ -0,0 +1,4 @@
+package tasks.manager.api.dto;
+
+public record Ping(Boolean status) {
+}
